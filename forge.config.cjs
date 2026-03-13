f6eacaff7223fee457818c4509557c67dc5d0b49
vxyi <vxyi@qq.com>
2026-03-13 20:20:09 +0800

    electron 打包

---
+++ forge.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    ignore: (path) => {
+      if (!path) return false;
+      const allowedPaths = [
+        '/dist',
+        '/main.js',
+        '/package.json',
+      ];
+
+      const isAllowed = allowedPaths.some(p => path.startsWith(p));
+      if (path.startsWith('/node_modules')) return true;
+
+      return !isAllowed;
+    },
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['win32'],
+    },
+  ],
+};
