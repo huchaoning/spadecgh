ebd7b28e92e23ae6eec3e2cde72ae077c7d30907
vxyi <vxyi@qq.com>
2026-06-09 15:09:14 +0800

    fix naming bugs

--- src/hduq/cnhu/cgh/_hook.py
+++ src/hduq/cnhu/cgh/_hook.py
@@ -87,7 +87,7 @@ def _serialize_cgh(cgh):
         'modes': []
     }
 
-    for mode, nx, ny in zip(cgh._mode_list, cgh.nx_list, cgh.ny_list):
+    for mode, nx, ny in zip(cgh._mode_list, cgh._nx_list, cgh._ny_list):
         mode_json = _serialize_mode(mode)
         mode_json['nx'] = float(nx)
         mode_json['ny'] = float(ny)
