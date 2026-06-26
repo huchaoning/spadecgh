6396251f5a7967fe9bcc54f75ff05eef2704e69b
vxyi <vxyi@qq.com>
2026-06-26 17:36:18 +0800

    fix app-icon

--- web_app/forge.config.cjs
+++ web_app/forge.config.cjs
@@ -50,7 +50,7 @@ if (process.platform === 'darwin') {
         name: '@reforged/maker-appimage',
         config: {
             compression: 'xz',
-            icon: 'icon/icon.png'
+            icon: 'icons/icon.png'
         },
     });
 }
